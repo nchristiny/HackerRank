556d2fa79c2205e059786187f4c3ae631f180ca3
nchristiny <icarus747@gmail.com>
2016-08-16 13:49:01 -0500

    Add refactor

--- algorithms/warmup/compare-the-triplets.rb
+++ algorithms/warmup/compare-the-triplets.rb
@@ -12,22 +12,16 @@ b2 = b2.to_i
 alice = 0
 bob = 0
 
-if a0 > b0
-  alice += 1
-elsif a0 < b0
-  bob += 1
+unless a0 == b0
+  a0 > b0 ? alice += 1 : bob += 1
 end
 
-if a1 > b1
-  alice += 1
-elsif a1 < b1
-  bob += 1
+unless a1 == b1
+  a1 > b1 ? alice += 1 : bob += 1
 end
 
-if a2 > b2
-  alice += 1
-elsif a2 < b2
-  bob += 1
+unless a2 == b2
+  a2 > b2 ? alice += 1 : bob += 1
 end
 
 puts "#{alice} #{bob}"
